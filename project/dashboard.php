566aade1af43e5f987a6372c2ed9b69a9e36c167
aniketjha1729 <kumaraniket1729@gmail.com>
2018-09-27 20:06:02 +0530

    dasboard

--- project/dashboard.php
+++ project/dashboard.php
@@ -12,7 +12,7 @@
         <html lang="en">
         <head>
             <title>Home</title>
-            <link rel="stylesheet" href="css/style2.css">
+            <link rel="stylesheet" href="css/dashboardstyle2.css">
         </head>
         <body>
         <?php
@@ -20,12 +20,38 @@
             $sql="SELECT * FROM login WHERE id='$details'";
             $data = mysqli_query($conn,$sql);
             $userDetails = mysqli_fetch_assoc($data);?>
-            
-            <div class="dash-header">
-                <h4 class="user"><?php echo "Welcome ".$userDetails['user_name'];?></h4>
+            <div class="header">            
                 <a class="logout"  href="logout.php">Logout</a>
+                <h4 class="h-4"><?php echo "Welcome ".$userDetails['user_name'];?></h4>              
             </div>
-            
+            <div class="container">
+               <div class="hello-left">
+                    <center>
+                   <textarea rows="25" cols="30" style="border-radius:10px"></textarea>
+                    </center>
+
+               </div>
+               <div class="hello-right">
+                   <center>
+                   <input type="text" class="inp" name="todo1"> <br>
+                   <input type="text" class="inp" name="todo2"> <br>
+                   <input type="text" class="inp" name="todo3"> <br>
+                   <input type="text" class="inp" name="todo4"> <br>
+                   <input type="text" class="inp" name="todo5"> <br>
+                   <input type="text" class="inp" name="todo6"> <br>
+                   <input type="submit" class="btn-submit" name="btn-submit" value="submit">
+                   <input type="submit" class="btn-del" name="btn-del" value="delete">
+                    </center>
+                   
+               </div>
+            </div>        
+
+            <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+                <script  src="js/index.js"></script>
+
+
+
+
         </body>
         </html>
         <?php
